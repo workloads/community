b86bd986d67c4face646eaaad798e0003f29d641
Kerim Satirli <k.satirli@gmail.com>
2023-08-29 10:05:15 +0200

    base provider configuration

---
+++ terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  # see https://developer.hashicorp.com/terraform/language/settings/terraform-cloud
+    cloud {
+      # see https://developer.hashicorp.com/terraform/cli/cloud/settings#organization
+      organization = "workloads"
+
+      # see https://developer.hashicorp.com/terraform/cli/cloud/settings#workspaces
+      workspaces {
+        name = "community"
+      }
+    }
+
+  # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
+  required_providers {
+    # see # see https://registry.terraform.io/providers/Lucky3028/discord/1.5.0
+    discord = {
+      source  = "Lucky3028/discord"
+      version = "1.5.0"
+    }
+  }
+
+  # see https://developer.hashicorp.com/terraform/language/settings#specifying-a-required-terraform-version
+  required_version = ">= 1.5.0, < 2.0.0"
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+# Application Identifiers can be retrieved by visiting https://discord.com/developers/applications/
+variable "discord_application_id" {
+  type        = string
+  description = "Discord Application Identifier."
+}
+
+variable "discord_application_permissions" {
+  type        = number
+  description = "Discord Application Permission Bit."
+  default     = 8 # `8` represents `Administrator` permissions
+}
+
+# Server Identifiers can be retrieved by inspect the Guilds API call for `discovery-metadata`:
+# see https://discord.com/api/v9/guilds/1025901757815070820/discovery-metadata
+variable "discord_server_id" {
+  type        = string
+  description = "Discord Server Identifier."
+}
+
+# Tokens may be created on https://discord.com/developers/applications/ by going to
+# "Applications", followed by "HashiCorp Terraform", then clicking "Bot" on the left
+# In the resulting view, the "Token" section offers options to create and reset tokens.
+#
+# See https://discordjs.guide/preparations/setting-up-a-bot-application.html#what-is-a-token-anyway for additional information.
+variable "discord_token" {
+  type        = string
+  description = "Discord API Token."
+}
+
